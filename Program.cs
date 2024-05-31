586138bc7cc35aa7587e5c3a88f88e0b15f9be2a
Diego Ramirez <alefrito554@gmail.com>
2024-05-31 18:09:15 -0500

    Prorgam

--- Program.cs
+++ Program.cs
@@ -24,7 +24,7 @@ builder.Services.AddControllers(config =>
 }
 
     );
-
+ 
 builder.Services.AddScoped<DbContext>(provider =>
 {
     var configuration = provider.GetRequiredService<IConfiguration>();
